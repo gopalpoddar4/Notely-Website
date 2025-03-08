7f7ddea8cd273926a3fe3af00529dac49f05837b
Gopal Poddar <g43142324@gmail.com>
2025-03-08 16:46:17 +0530

    initial coomit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('downloadBtn').addEventListener('click', function() {
+    alert('Download link will be available soon!');
+    // Yahan aap download link ya logic add kar sakte hain
+});
